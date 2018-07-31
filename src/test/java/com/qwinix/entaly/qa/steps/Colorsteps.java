15feddce8e29e18797a2c8d14d6619424e1167e6
deepikah56 <dhonnagirigowda@qwinix.io>
2018-07-31 10:29:40 +0530

    change

--- src/test/java/com/qwinix/entaly/qa/steps/Colorsteps.java
+++ src/test/java/com/qwinix/entaly/qa/steps/Colorsteps.java
@@ -3,6 +3,7 @@ package com.qwinix.entaly.qa.steps;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.Color;
 
 import com.qwinix.entaly.qa.StepsDefinitions;
 
@@ -19,18 +20,23 @@ public class Colorsteps {
 	public void verifycolor() throws InterruptedException {
 //		String color = driver.findElement(By.xpath("//h1[text()='text formatting']")).getCssValue("color");
 //		System.out.println(color);
-		
-		String color = driver.findElement(By.xpath("//h1[text()='text formatting']")).getCssValue("color").trim();    
-		 System.out.println("RGB_Color: " + color);  
-
-		 //  RGB to HEX   
-		 String color_hex[];  
-		 color_hex = color.replace("rgba(", "").split(",");       
-		 String actual_hex = String.format("#%02x%02x%02x", Integer.parseInt(color_hex[0].trim()), Integer.parseInt(color_hex[1].trim()), Integer.parseInt(color_hex[2].trim()));  
-
-		// further can verify with Actual hex value with Expected hex value  
-		 Assert.assertEquals("actual_hex should equal to: ", "#4caf50", actual_hex);
-		 System.out.println("hex_Color: " + actual_hex);  
+//		
+//		String color = driver.findElement(By.xpath("//h1[text()='text formatting']")).getCssValue("color").trim();    
+//		 System.out.println("RGB_Color: " + color);  
+//
+//		 //  RGB to HEX   
+//		 String color_hex[];  
+//		 color_hex = color.replace("rgba(", "").split(",");       
+//		 String actual_hex = String.format("#%02x%02x%02x", Integer.parseInt(color_hex[0].trim()), Integer.parseInt(color_hex[1].trim()), Integer.parseInt(color_hex[2].trim()));  
+//
+//		// further can verify with Actual hex value with Expected hex value  
+//		 Assert.assertEquals("actual_hex should equal to: ", "#4caf50", actual_hex);
+//		 System.out.println("hex_Color: " + actual_hex);  
+		  
+		 String color = driver.findElement(By.xpath("//h1[text()='text formatting']")).getCssValue("color");
+		 System.out.println(color);
+		 String hex = Color.fromString(color).asHex();
+		 System.out.println(hex);
 	}
 
 }
