23e2fe08632fb4afa370193b221cc45c3bc1b907
deepikah56 <dhonnagirigowda@qwinix.io>
2018-08-02 09:57:14 +0530

    drag and drop

--- src/test/java/com/qwinix/entaly/qa/steps/Dragdropsteps.java
+++ src/test/java/com/qwinix/entaly/qa/steps/Dragdropsteps.java
@@ -3,7 +3,6 @@ package com.qwinix.entaly.qa.steps;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 import com.qwinix.entaly.qa.StepsDefinitions;
@@ -13,21 +12,21 @@ public class Dragdropsteps {
 	
 	public void openurl() throws InterruptedException {
 		driver.manage().window().maximize();
-		driver.get("http://www.seleniumeasy.com/test/drag-and-drop-demo.html");
+//		driver.get("http://www.seleniumeasy.com/test/drag-and-drop-demo.html");
+//		driver.get("https://html5demos.com/drag/#");
+		driver.get("https://demos.telerik.com/kendo-ui/listbox/drag-and-drop");
+
 		Thread.sleep(3000);
 	}
 	
 	public void draganddrop() throws InterruptedException {
-	  
-	    WebElement From = driver.findElement(By.xpath("//span[text()='Draggable 1']"));
-	    WebElement To = driver.findElement(By.xpath("//*[@id='mydropzone']"));
-	    Actions builder = new Actions(driver);
-	    Action dragAndDrop = builder.clickAndHold(From)
-	    .moveToElement(To)
-	    .release(To)
-	    .build();
-	    dragAndDrop.perform();
-	    Thread.sleep(3000);
+	    driver.findElement(By.xpath(".//*[@id='example']/div/div[1]/div/ul/li[1]")).click();
+        WebElement elementToMove = driver.findElement(By.xpath(".//*[@id='example']/div/div[1]/div/ul/li[1]"));
+        WebElement moveToElement = driver.findElement(By.xpath(".//*[@id='example']/div/div[2]/div/ul/li[4]"));
+        Actions builder = new Actions(driver);
+        builder.dragAndDrop(elementToMove, moveToElement).build().perform();
+        Thread.sleep(3000);
+        
 }
 
 }
